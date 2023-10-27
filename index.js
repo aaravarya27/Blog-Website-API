9168adb44f1560c87d7e7b8659c4bb8ca47fc187
Aarav Arya <aarav.arya2307@gmail.com>
2023-10-27 15:58:23 +0530

    node packages

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ let posts = [
     content:
       "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
     author: "Alex Thompson",
-    date: "2023-08-01T10:00:00Z",
+    date: "18/08/2022, 3:54:18 pm",
   },
   {
     id: 2,
@@ -96,7 +96,7 @@ let posts = [
     content:
       "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
     author: "Mia Williams",
-    date: "2023-08-05T14:30:00Z",
+    date: "27/06/2022, 2:17:02 am",
   },
   {
     id: 3,
@@ -104,6 +104,6 @@ let posts = [
     content:
       "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
     author: "Samuel Green",
-    date: "2023-08-10T09:15:00Z",
+    date: "27/08/2022, 7:20:11 pm",
   },
 ];
